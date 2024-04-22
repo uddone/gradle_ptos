5b1967a19def56428d450f07ef12efa2d465a9ba
uddone <zzwbsldj@naver.com>
2024-04-22 13:01:34 +0900

    fix : dotenv 설치로 인한 수정

--- src/main/java/com/gsil/gradleptos/GradlePtosApplication.java
+++ src/main/java/com/gsil/gradleptos/GradlePtosApplication.java
@@ -1,5 +1,6 @@
 package com.gsil.gradleptos;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradlePtosApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// 환경 변수를 시스템 환경 변수로 설정
+		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USER"));
+		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(GradlePtosApplication.class, args);
 	}
 }
+/*
+*
+* Dotenv dotenv = Dotenv.load();
+		String dbUrl = dotenv.get("DATABASE_URL");
+		String dbUser = dotenv.get("DATABASE_USER");
+		String dbPassword = dotenv.get("DATABASE_PASSWORD");
+*
+* */
